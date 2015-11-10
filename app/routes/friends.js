73cd35afcac8402fcdff0f5e435c8c25875728f8
Hussien <akhayoon@gmail.com>
2015-11-10 13:47:39 -0800

    added ability to save a friend using ember data and some validation

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  
+  action: {
+    save() {
+      console.log('+--- save action bubbled up to friends route');
+
+      return true;
+    },
+    cancel() {
+      console.log('+--- cancel action bubbled up to friends route');
+
+      return true;
+    }
+  }
+
 });
