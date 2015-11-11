a4e26bfdaab6f162541d6246bd497cd912ca0ddc
Hussien <akhayoon@gmail.com>
2015-11-11 14:53:06 -0800

    added header partial along with new articles resource

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,11 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  email: DS.attr('string'),
-  twitter: DS.attr('string'),
-  totalArticles: DS.attr('number'),
+  firstName:      DS.attr('string'),
+  lastName:       DS.attr('string'),
+  email:          DS.attr('string'),
+  twitter:        DS.attr('string'),
+  totalArticles:  DS.attr('number'),
+  articles:       DS.hasMany('article'),
   fullName: Ember.computed('firstName', 'lastName', {
     get() {
       return this.get('firstName') + ' ' + this.get('lastName');
