d3aeaa35e5ca90a38a5f1ca7d56d826c997525f2
Hussien <akhayoon@gmail.com>
2015-11-11 14:05:23 -0800

    refactored delte action to be called from application route, also add fontello and picnic

--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    cancel(){
+      console.log('action bubbled up to edit route');
+      return true;
+    }
+  }
 });
